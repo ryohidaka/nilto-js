db0672bc4857e9f5fe89aee710846f350b766314
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-10-15 08:40:01 +0900

    feat: 動作確認用の一覧表示を追加

--- src/example/main.ts
+++ src/example/main.ts
@@ -1,9 +1,48 @@
 import { Nilto } from "..";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div></div>
+  <div>
+    <table id="articles-table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>CreatedAt</th>
+          <th>PublishedAt</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
+  </div>
 `;
 
 const apiKey = import.meta.env.VITE_NILTO_API_KEY;
-
 const client = new Nilto(apiKey);
+
+const contents = await client.getContents({ model: "blog_articles" });
+const tableBody = document.querySelector<HTMLTableSectionElement>(
+  "#articles-table tbody",
+);
+
+// 汎用的なセル生成関数
+const createCell = (content: string) => {
+  const cell = document.createElement("td");
+  cell.textContent = content;
+  return cell;
+};
+
+// テーブルに行を追加
+contents.data.forEach(({ _id, title, _created_at, _published_at, _status }) => {
+  const row = document.createElement("tr");
+  const cells = [
+    createCell(_id),
+    createCell(title),
+    createCell(new Date(_created_at).toLocaleDateString()),
+    createCell(new Date(_published_at).toLocaleDateString()),
+    createCell(_status),
+  ];
+
+  cells.forEach((cell) => row.appendChild(cell));
+  tableBody?.appendChild(row);
+});
