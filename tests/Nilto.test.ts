9f26e17dc99245d99d13656dba3c6266da0fa59d
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-10-15 09:29:59 +0900

    test: getContent() のテストコードを追加

--- tests/Nilto.test.ts
+++ tests/Nilto.test.ts
@@ -21,4 +21,23 @@ describe("Nilto", () => {
     const contents = await client.getContents({ model: "sample" });
     expect(contents).toEqual(mockResponse);
   });
+
+  it("getContent", async () => {
+    // モックレスポンス設定
+    const mockResponse = {
+      _id: 123456,
+      _model: "sample",
+      _status: "published",
+      _title: "sample",
+    };
+
+    (global.fetch as Mock).mockResolvedValue({
+      ok: true,
+      json: vi.fn().mockResolvedValue(mockResponse),
+    });
+
+    const client = new Nilto("test-key");
+    const content = await client.getContentsID("123456");
+    expect(content).toEqual(mockResponse);
+  });
 });
