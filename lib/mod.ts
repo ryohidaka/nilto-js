15a0d8e7461ec1a0cbb9e2d7eaf375ab71d9d6e3
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-08-02 23:59:18 +0900

    feat: Nilto クラスの初期化処理を追加

--- lib/mod.ts
+++ lib/mod.ts
@@ -1,4 +1,22 @@
 /**
  * NILTO SDK for JavaScript
+ *
+ * @example 初期化
+ * ```ts
+ * import { Nilto } from '@hidaka/nilto';
+ *
+ * const nilto = new Nilto('your-api-key-here');
+ * ```
  */
-export class Nilto {}
+export class Nilto {
+  private apiKey: string;
+
+  /**
+   * Niltoクラスの初期化処理.
+   *
+   * @param apiKey - NILTO CMS APIへのリクエストを認証するために必要なAPIキー。
+   */
+  constructor(apiKey: string) {
+    this.apiKey = apiKey;
+  }
+}
