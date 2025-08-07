bed3f9a87ba574ccb60cc84b18e6de4813ca3227
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2025-08-07 13:12:37 +0900

    ci: パッケージ公開用のGitHub Actions ワークフローを追加

---
+++ scripts/build_npm.ts
@@ -0,0 +1,39 @@
+// @see https://github.com/denoland/dnt
+
+import { build, emptyDir } from "@deno/dnt";
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  shims: {
+    // see JS docs for overview and more options
+    deno: true,
+  },
+  package: {
+    // package.json properties
+    name: "nilto",
+    version: Deno.args[0]?.replace(/^v/, ""),
+    description: "JavaScript SDK Client for NILTO.",
+    keywords: [],
+    license: "MIT",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/ryohidaka/nilto-js.git",
+    },
+    author: "ryohidaka",
+    bugs: {
+      url: "https://github.com/ryohidaka/nilto-js/issues",
+    },
+    homepage: "https://github.com/ryohidaka/nilto-js#readme",
+  },
+  compilerOptions: {
+    lib: ["ESNext"],
+  },
+  postBuild() {
+    // steps to run after building and before running the tests
+    Deno.copyFileSync("LICENSE", "npm/LICENSE");
+    Deno.copyFileSync("README.md", "npm/README.md");
+  },
+});
